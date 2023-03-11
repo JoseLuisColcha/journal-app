b894c11bf92f785151135d51aa7e44944feedd68
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-10 20:58:05 -0500

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { JournalApp } from "./JournalApp";
-import "./styles/style.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { JournalApp } from './JournalApp'
+import './styles/style.scss'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<JournalApp />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<JournalApp />)
