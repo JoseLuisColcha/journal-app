6b6e314e5e7010d1825f33c7ffadee319976647e
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-24 17:12:38 -0500

    Add global loading & conserve auth state

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -6,8 +7,33 @@ import {
 } from 'react-router-dom'
 import { JournalPage } from '../components/journal/JournalPage'
 import { AuthRouter } from './AuthRouter'
+import { firebase } from '../firebase/firebaseConfig'
+import { useDispatch } from 'react-redux'
+import { login } from '../actions/auth'
 
 export const AppRouter = () => {
+	const dispatch = useDispatch()
+
+	const [checking, setChecking] = useState(true)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user?.uid) {
+				dispatch(login(user.uid, user.displayName))
+				setIsLoggedIn(true)
+			} else {
+				setIsLoggedIn(false)
+			}
+
+			setChecking(false)
+		})
+	}, [dispatch, setChecking, setIsLoggedIn])
+
+	if (checking) {
+		return <h1>Espere...</h1>
+	}
+
 	return (
 		<Router>
 			<div>
